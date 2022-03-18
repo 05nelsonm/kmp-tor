fb8ce20843dcf5f8edef9c6b3f4b98df078075ca
Matthew Nelson <developer@matthewnelson.io>
2022-03-18 06:39:50 -0400

    fix maven central username/password property retrieval

--- samples/android/build.gradle.kts
+++ samples/android/build.gradle.kts
@@ -27,8 +27,8 @@ if (env.kmpTorBinaries.pollStagingRepo) {
     repositories {
         maven("https://oss.sonatype.org/content/groups/staging") {
             credentials {
-                username = ext.get("mavenCentralUsername").toString()
-                password = ext.get("mavenCentralPassword").toString()
+                username = rootProject.ext.get("mavenCentralUsername").toString()
+                password = rootProject.ext.get("mavenCentralPassword").toString()
             }
         }
     }
