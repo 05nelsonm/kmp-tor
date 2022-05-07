a0a146d0296dcf2e67317831ba32785ed1b11985
Matthew Nelson <developer@matthewnelson.io>
2022-05-07 13:22:10 -0400

    Prepare for next development iteration

--- buildSrc/src/main/kotlin/kmp/tor/env.kt
+++ buildSrc/src/main/kotlin/kmp/tor/env.kt
@@ -18,9 +18,9 @@ package kmp.tor
 @Suppress("ClassName")
 object env {
     // Must be either "-SNAPSHOT" || ""
-    private const val SNAPSHOT              = ""
+    private const val SNAPSHOT              = "-SNAPSHOT"
 
-    private const val MANAGER_VERSION_NAME  = "0.1.1"
+    private const val MANAGER_VERSION_NAME  = "0.1.2"
     //                           1.0.0-alpha1  == 01_00_00_11
     //                           1.0.0-alpha2  == 01_00_00_12
     //                           1.0.0-beta1   == 01_00_00_21
@@ -29,7 +29,7 @@ object env {
     //                           1.0.1         == 01_00_01_99
     //                           1.1.1         == 01_01_01_99
     //                           1.15.1        == 01_15_01_99
-    private const val MANAGER_VERSION_CODE  = /*00_0*/1_01_99
+    private const val MANAGER_VERSION_CODE  = /*00_0*/1_02_99
 
     private const val BINARY_VERSION_NAME   = "0.4.6.10"
     //                           0.4.6.9       == 00_04_06_09_00
