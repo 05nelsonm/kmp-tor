6a93a26ab88e9f9355444443a1fc5d16f39ca6d6
Matthew Nelson <developer@matthewnelson.io>
2022-05-31 06:23:06 -0400

    fix minSdk version (publication check tool)

--- tools/check-publication/build.gradle.kts
+++ tools/check-publication/build.gradle.kts
@@ -50,7 +50,7 @@ kmpConfiguration {
             KmpTarget.Jvm.Android(
                 buildTools = versions.android.buildTools,
                 compileSdk = versions.android.sdkCompile,
-                minSdk = versions.android.sdkMin16,
+                minSdk = versions.android.sdkMin21,
                 mainSourceSet = {
                     dependencies {
                         implementation("${pConfig.group}:kmp-tor:${env.kmpTorAll.version.name}")
