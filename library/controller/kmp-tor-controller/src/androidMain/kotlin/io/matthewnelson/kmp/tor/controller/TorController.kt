a37bfc604cd5e59bd4c42b8a8e0ef62faa867097
Matthew Nelson <developer@matthewnelson.io>
2022-06-21 09:03:28 -0400

    change JvmName for file to androidTorController

--- library/controller/kmp-tor-controller/src/androidMain/kotlin/io/matthewnelson/kmp/tor/controller/TorController.kt
+++ library/controller/kmp-tor-controller/src/androidMain/kotlin/io/matthewnelson/kmp/tor/controller/TorController.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  **/
-@file:JvmName("jvmTorController")
+@file:JvmName("androidTorController")
 
 package io.matthewnelson.kmp.tor.controller
 
