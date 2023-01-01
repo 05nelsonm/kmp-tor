e20526aa17d280e3175313efd9d259ea1c5245de
Matthew Nelson <developer@matthewnelson.io>
2023-01-01 07:53:33 -0500

    Prepare for next development iteration

--- buildSrc/src/main/kotlin/kmp/tor/env.kt
+++ buildSrc/src/main/kotlin/kmp/tor/env.kt
@@ -18,9 +18,9 @@ package kmp.tor
 @Suppress("ClassName")
 object env {
     // Must be either "-SNAPSHOT" || ""
-    private const val SNAPSHOT              = ""
+    private const val SNAPSHOT              = "-SNAPSHOT"
 
-    private const val MANAGER_VERSION_NAME  = "1.3.1"
+    private const val MANAGER_VERSION_NAME  = "1.3.2"
     //                       1.0.0-alpha1  ==     01_00_00_11
     //                       1.0.0-alpha2  ==     01_00_00_12
     //                       1.0.0-beta1   ==     01_00_00_21
@@ -29,7 +29,7 @@ object env {
     //                       1.0.1         ==     01_00_01_99
     //                       1.1.1         ==     01_01_01_99
     //                       1.15.1        ==     01_15_01_99
-    private const val MANAGER_VERSION_CODE  = /*0*/1_03_01_99
+    private const val MANAGER_VERSION_CODE  = /*0*/1_03_02_99
 
     private const val BINARY_VERSION_NAME   = "4.7.12-0"
     //                       4.6.9-0       ==     00_04_06_09_00
@@ -80,7 +80,7 @@ object env {
          * Indicates that the release is only bumping Tor versions,
          * and to hold publication for all other modules.
          * */
-        const val isBinaryRelease           = true
+        const val isBinaryRelease           = false
         const val holdPublication           = false
 
         object version {
