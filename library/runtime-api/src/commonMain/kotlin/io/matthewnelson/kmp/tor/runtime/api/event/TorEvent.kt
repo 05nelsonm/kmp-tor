bf2bd11c3d2d5fb0f30dfd43eec3a379724afde0
Matthew Nelson <developer@matthewnelson.io>
2024-01-05 11:22:02 -0500

    Add event listeners

--- library/runtime-api/src/commonMain/kotlin/io/matthewnelson/kmp/tor/runtime/api/event/TorEvent.kt
+++ library/runtime-api/src/commonMain/kotlin/io/matthewnelson/kmp/tor/runtime/api/event/TorEvent.kt
@@ -13,11 +13,22 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  **/
+@file:Suppress("SpellCheckingInspection")
+
 package io.matthewnelson.kmp.tor.runtime.api.event
 
+import io.matthewnelson.kmp.tor.core.api.annotation.InternalKmpTorApi
+import io.matthewnelson.kmp.tor.core.resource.SynchronizedObject
+import io.matthewnelson.kmp.tor.core.resource.synchronized
+import io.matthewnelson.kmp.tor.runtime.api.ItBlock
+import kotlin.jvm.JvmField
 
 /**
  * [Asynchronous Events](https://torproject.gitlab.io/torspec/control-spec/#asynchronous-events)
+ *
+ * @see [Listener]
+ * @see [listener]
+ * @see [Processor]
  * */
 public enum class TorEvent {
 
@@ -179,4 +190,162 @@ public enum class TorEvent {
 //     * [Pluggable Transport Status](https://torproject.gitlab.io/torspec/control-spec/#pluggable-transport-status)
 //     * */
 //    PT_STATUS,
+
+    /**
+     * Create a listener for a given [TorEvent]
+     *
+     * e.g. (Kotlin)
+     *
+     *     val bwListener = TorEvent.BW.newListener { event ->
+     *         updateNotification(event.formatBandwidth())
+     *     }
+     *
+     * e.g. (Java)
+     *
+     *     TorEvent.Listener bwListener = TorEvent.BW.newListener(e -> {
+     *         updateNotification(formatBandwidth(e));
+     *     });
+     *
+     * @param [block] the callback to pass the event text to
+     * */
+    public fun listener(
+        block: ItBlock<String>,
+    ): Listener = Listener(null,this, block)
+
+    /**
+     * Create a listener for a given [TorEvent] and [tag].
+     *
+     * This is useful for lifecycle aware components, all of which
+     * can be removed with a single call using the [tag] upon
+     * component destruction.
+     *
+     * e.g. (Kotlin)
+     *
+     *     val bwListener = TorEvent.BW.newListener("my service") { event ->
+     *         updateNotification(event.formatBandwidth())
+     *     }
+     *
+     * e.g. (Java)
+     *
+     *     TorEvent.Listener bwListener = TorEvent.BW.newListener("my service", e -> {
+     *         updateNotification(formatBandwidth(e));
+     *     });
+     *
+     * @param [tag] Any non-blank string value
+     * @param [block] the callback to pass the event text to
+     * */
+    public fun listener(
+        tag: String,
+        block: ItBlock<String>,
+    ): Listener = Listener(tag.ifBlank { null },this, block)
+
+    public open class Listener(
+        @JvmField
+        public val tag: String?,
+        @JvmField
+        public val event: TorEvent,
+        @JvmField
+        public val block: ItBlock<String>,
+    )
+
+    /**
+     * Base abstraction for implementations that process [TorEvent].
+     * */
+    public abstract class Processor
+    @InternalKmpTorApi
+    protected constructor() {
+
+        private val listeners = mutableSetOf<Listener>()
+
+        @OptIn(InternalKmpTorApi::class)
+        private val lock = SynchronizedObject()
+
+        /**
+         * Add a single [Listener].
+         * */
+        public fun add(listener: Listener) {
+            withListeners { add(listener) }
+        }
+
+        /**
+         * Add multiple [Listener].
+         * */
+        public fun add(vararg listeners: Listener) {
+            if (listeners.isEmpty()) return
+            withListeners { listeners.forEach { add(it) } }
+        }
+
+        /**
+         * Remove a single [Listener].
+         * */
+        public fun remove(listener: Listener) {
+            withListeners { remove(listener) }
+        }
+
+        /**
+         * Remove multiple [Listener].
+         * */
+        public fun remove(vararg listeners: Listener) {
+            if (listeners.isEmpty()) return
+            withListeners { listeners.forEach { remove(it) } }
+        }
+
+        /**
+         * Remove all [Listener] for given [event]
+         * */
+        public open fun removeAll(event: TorEvent) {
+            withListeners {
+                val listeners = filter { it.event == event }
+                removeAll(listeners.toSet())
+            }
+        }
+
+        /**
+         * Remove all [Listener] for given [events]
+         * */
+        public fun removeAll(vararg events: TorEvent) {
+            if (events.isEmpty()) return
+            withListeners {
+                val listeners = filter { events.contains(it.event) }
+                removeAll(listeners.toSet())
+            }
+        }
+
+        /**
+         * Remove all [Listener] for a given [tag]
+         * */
+        public open fun removeAll(tag: String) {
+            withListeners {
+                val listeners = filter { it.tag == tag }
+                removeAll(listeners.toSet())
+            }
+        }
+
+        /**
+         * Removes all [Listener] for all [TorEvent]s.
+         * */
+        public open fun clearListeners() {
+            withListeners { clear() }
+        }
+
+        protected fun notify(event: TorEvent, output: String) {
+            withListeners {
+                forEach { listener ->
+                    if (listener.event != event) return@forEach
+                    listener.block.invoke(output)
+                }
+            }
+        }
+
+        protected fun <T: Any?> withListeners(
+            block: MutableSet<Listener>.() -> T,
+        ): T {
+            @OptIn(InternalKmpTorApi::class)
+            val result = synchronized(lock) {
+                block(listeners)
+            }
+
+            return result
+        }
+    }
 }
