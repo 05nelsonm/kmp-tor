fec9ae26e4814e10be512843acd4dd3635fef1ba
Matthew Nelson <developer@matthewnelson.io>
2024-01-26 18:26:04 -0500

    Add more fallbacks and default IPv4/IPv6 addresses

--- library/runtime-ctrl-api/src/jsMain/kotlin/io/matthewnelson/kmp/tor/runtime/ctrl/api/internal/JsPlatform.kt
+++ library/runtime-ctrl-api/src/jsMain/kotlin/io/matthewnelson/kmp/tor/runtime/ctrl/api/internal/JsPlatform.kt
@@ -24,6 +24,8 @@ import io.matthewnelson.kmp.tor.core.resource.OSInfo
 import io.matthewnelson.kmp.tor.runtime.ctrl.api.address.IPAddress
 import io.matthewnelson.kmp.tor.runtime.ctrl.api.address.IPAddress.Companion.toIPAddress
 import io.matthewnelson.kmp.tor.runtime.ctrl.api.address.IPAddress.Companion.toIPAddressOrNull
+import io.matthewnelson.kmp.tor.runtime.ctrl.api.address.IPAddress.V4.Companion.toIPAddressV4
+import io.matthewnelson.kmp.tor.runtime.ctrl.api.address.IPAddress.V6.Companion.toIPAddressV6
 import io.matthewnelson.kmp.tor.runtime.ctrl.api.address.LocalHost
 
 @OptIn(InternalKmpTorApi::class)
@@ -44,31 +46,44 @@ internal actual val ProcessID: Int? get() = process_pid
 
 @Suppress("ACTUAL_ANNOTATIONS_NOT_MATCH_EXPECT")
 internal actual fun LocalHost.Companion.resolveAll(): Set<IPAddress> {
-    val interfaces = try {
-        os_networkInterfaces()
-    } catch (t: Throwable) {
-        throw IOException("Failed to resolve network interfaces", t)
-    }
-
-    val interfacesValues = try {
-        objectValues(interfaces)
-    } catch (t: Throwable) {
-        throw IOException("Failed to acquire object values", t)
-    }
-
     val set = LinkedHashSet<IPAddress>(2, 1.0F)
 
-    interfacesValues.forEach { values ->
-        values.forEach values@ { entry ->
-            if (entry.internal as Boolean) {
-                set.add((entry.address as String).toIPAddress())
-            }
-        }
+    tryOsInterfaces(set)
+    if (set.hasIPv4IPv6) return set
+
+    if (!IsUnixLikeHost) {
+        // Windows... fake it till we make it
+        set.add("127.0.0.1".toIPAddressV4())
+        set.add("::1".toIPAddressV6())
+        return set
     }
 
-    set.addEtcHostsIfNeeded()
+    // Try some shell commands
+    tryParseIFConfig(set)
+    if (set.hasIPv4IPv6) return set
 
+    // last resort. Read from /etc/hosts
+    tryParseEtcHosts(set)
+    if (set.hasIPv4IPv6) return set
+
+    // More faking it...
+    set.add("127.0.0.1".toIPAddressV4())
+    set.add("::1".toIPAddressV6())
     return set
+}
+
+private fun tryOsInterfaces(set: LinkedHashSet<IPAddress>) {
+    try {
+        objectValues(os_networkInterfaces()).forEach { values ->
+            values.forEach values@ { entry ->
+                if (entry.internal as Boolean) {
+                    set.add((entry.address as String).toIPAddress())
+                }
+            }
+        }
+    } catch (_: Throwable) {
+        return
+    }
 }
 
 @Suppress("NOTHING_TO_INLINE")
@@ -76,9 +91,8 @@ private inline fun objectValues(jsObject: dynamic): Array<Array<dynamic>> {
     return js("Object").values(jsObject).unsafeCast<Array<Array<dynamic>>>()
 }
 
-private fun LinkedHashSet<IPAddress>.addEtcHostsIfNeeded() {
-    if (!IsUnixLikeHost) return
-
+@Suppress("NOTHING_TO_INLINE")
+private inline val LinkedHashSet<IPAddress>.hasIPv4IPv6: Boolean get() {
     var hasIPv4 = false
     var hasIPv6 = false
     forEach { address ->
@@ -87,16 +101,55 @@ private fun LinkedHashSet<IPAddress>.addEtcHostsIfNeeded() {
             is IPAddress.V6 -> hasIPv6 = true
         }
     }
-    if (hasIPv4 && hasIPv6) return
+    return hasIPv4 && hasIPv6
+}
 
+private fun tryParseIFConfig(set: LinkedHashSet<IPAddress>) {
+    val ifConfig = try {
+        val buffer = child_process_execSync("ifconfig")
+        @OptIn(DelicateFileApi::class)
+        Buffer.wrap(buffer).toUtf8().trimIndent()
+    } catch (_: Throwable) {
+        return
+    }
+
+    val configs = mutableListOf<MutableList<String>>()
+    ifConfig.lines().forEach { line ->
+        if (line.isBlank()) return@forEach
+        if (!line.startsWith(' ')) {
+            configs.add(mutableListOf(line.trim()))
+            return@forEach
+        }
+        configs.first().add(line.trim())
+    }
+
+    configs.forEach configs@ { config ->
+        if (config.isEmpty()) return@configs
+
+        val i = config.iterator()
+        if (!i.next().contains("LOOPBACK")) return@configs
+
+        // look for inet and inet6
+        while (i.hasNext()) {
+            val line = i.next()
+            if (!line.startsWith("inet")) continue
+            val address = line.substringAfter(' ')
+                .substringBefore(' ')
+                .toIPAddressOrNull()
+                ?: continue
+            set.add(address)
+        }
+    }
+}
+
+private fun tryParseEtcHosts(set: LinkedHashSet<IPAddress>) {
     val etcHosts = "/etc/hosts".toFile()
     if (!etcHosts.exists()) return
 
     val lines = try {
         etcHosts.readUtf8().lines()
     } catch (t: Throwable) {
-        @OptIn(DelicateFileApi::class)
-        throw t.toIOException()
+        return
     }
 
     for (line in lines) {
@@ -107,6 +160,6 @@ private fun LinkedHashSet<IPAddress>.addEtcHostsIfNeeded() {
             .toIPAddressOrNull()
             ?: continue
 
-        add(address)
+        set.add(address)
     }
 }
