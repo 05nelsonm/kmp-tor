87a450cf08e69e9af7eff436b5e409297447cb65
Matthew Nelson <developer@matthewnelson.io>
2024-04-27 18:18:49 -0400

    Shorten Util file names

--- library/runtime-core/src/nonJsMain/kotlin/io/matthewnelson/kmp/tor/runtime/core/util/JobUtil.kt
+++ library/runtime-core/src/nonJsMain/kotlin/io/matthewnelson/kmp/tor/runtime/core/util/JobUtil.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  **/
-@file:JvmName("QueuedJobUtil")
+@file:JvmName("JobUtil")
 
 package io.matthewnelson.kmp.tor.runtime.core.util
 
--- library/runtime/src/nonJsMain/kotlin/io/matthewnelson/kmp/tor/runtime/util/ActionUtil.kt
+++ library/runtime/src/nonJsMain/kotlin/io/matthewnelson/kmp/tor/runtime/util/ActionUtil.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  **/
-@file:JvmName("RuntimeActionUtil")
+@file:JvmName("ActionUtil")
 @file:Suppress("KotlinRedundantDiagnosticSuppress")
 
 package io.matthewnelson.kmp.tor.runtime.util
