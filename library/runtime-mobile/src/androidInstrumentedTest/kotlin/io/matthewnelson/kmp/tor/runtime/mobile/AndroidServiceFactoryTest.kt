856e3bc9139be805242daa21a635ad2c02f5adb6
Matthew Nelson <developer@matthewnelson.io>
2024-05-05 13:20:29 -0400

    Add Android test for multi-instance service operations

--- library/runtime-mobile/src/androidInstrumentedTest/kotlin/io/matthewnelson/kmp/tor/runtime/mobile/AndroidServiceFactoryTest.kt
+++ library/runtime-mobile/src/androidInstrumentedTest/kotlin/io/matthewnelson/kmp/tor/runtime/mobile/AndroidServiceFactoryTest.kt
@@ -17,8 +17,7 @@ package io.matthewnelson.kmp.tor.runtime.mobile
 
 import android.app.Application
 import androidx.test.core.app.ApplicationProvider
-import io.matthewnelson.kmp.tor.core.api.ResourceInstaller
-import io.matthewnelson.kmp.tor.core.api.ResourceInstaller.Paths
+import io.matthewnelson.kmp.process.Blocking
 import io.matthewnelson.kmp.tor.resource.tor.TorResources
 import io.matthewnelson.kmp.tor.runtime.Action.Companion.startDaemonSync
 import io.matthewnelson.kmp.tor.runtime.Action.Companion.stopDaemonSync
@@ -26,6 +25,7 @@ import io.matthewnelson.kmp.tor.runtime.Lifecycle
 import io.matthewnelson.kmp.tor.runtime.RuntimeEvent
 import io.matthewnelson.kmp.tor.runtime.TorRuntime
 import kotlin.test.*
+import kotlin.time.Duration.Companion.milliseconds
 
 class AndroidServiceFactoryTest {
 
@@ -56,29 +56,80 @@ class AndroidServiceFactoryTest {
 
         val lces = mutableListOf<Lifecycle.Event>()
         val factory = TorRuntime.Builder(environment) {
-            val lock = Any()
             observerStatic(RuntimeEvent.LIFECYCLE) {
 //                println(it)
-                synchronized(lock) { lces.add(it) }
+                synchronized(lces) { lces.add(it) }
             }
 //            observerStatic(RuntimeEvent.LOG.DEBUG) { println(it) }
         }
 
         factory.startDaemonSync().stopDaemonSync()
 
-        var containsUnbind = false
-        var containsDestroy = false
-        for (event in lces) {
-            if (event.className != "TorService") continue
+        lces.assertContains("TorService", Lifecycle.Event.Name.OnUnbind)
+        lces.assertContains("TorService", Lifecycle.Event.Name.OnDestroy)
+    }
 
-            if (event.name == Lifecycle.Event.Name.OnUnbind) {
-                containsUnbind = true
+    @Test
+    fun givenTorService_whenMultipleRuntime_thenServiceIsDestroyedWhenLastRuntimeDestroyed() {
+        val env1 = app.createTorRuntimeEnvironment { dir -> TorResources(dir) }
+        val env2 = app.createTorRuntimeEnvironment(dirName = "torservice2") { dir -> TorResources(dir) }
+        env1.debug = true
+        env2.debug = true
+
+        val lces1 = mutableListOf<Lifecycle.Event>()
+        val factory1 = TorRuntime.Builder(env1) {
+            observerStatic(RuntimeEvent.LIFECYCLE) {
+//                println(it)
+                synchronized(lces1) { lces1.add(it) }
             }
-            if (event.name == Lifecycle.Event.Name.OnDestroy) {
-                containsDestroy = true
-            }
+//            observerStatic(RuntimeEvent.LOG.DEBUG) { println(it) }
         }
-        assertTrue(containsUnbind)
-        assertTrue(containsDestroy)
+
+        val lces2 = mutableListOf<Lifecycle.Event>()
+        val factory2 = TorRuntime.Builder(env2) {
+            observerStatic(RuntimeEvent.LIFECYCLE) {
+//                println(it)
+                synchronized(lces2) { lces2.add(it) }
+            }
+//            observerStatic(RuntimeEvent.LOG.DEBUG) { println(it) }
+        }
+
+        factory1.startDaemonSync()
+        factory2.startDaemonSync()
+
+        factory1.stopDaemonSync()
+        lces1.assertContains("RealTorRuntime", Lifecycle.Event.Name.OnDestroy)
+        lces1.assertContains("TorService", Lifecycle.Event.Name.OnUnbind)
+
+        lces1.assertDoesNotContain("TorService", Lifecycle.Event.Name.OnDestroy)
+        lces2.assertDoesNotContain("TorService", Lifecycle.Event.Name.OnDestroy)
+
+        factory2.stopDaemonSync()
+        lces1.assertContains("TorService", Lifecycle.Event.Name.OnDestroy)
+
+        lces2.assertContains("RealTorRuntime", Lifecycle.Event.Name.OnDestroy)
+        lces2.assertContains("TorService", Lifecycle.Event.Name.OnUnbind)
+        lces2.assertContains("TorService", Lifecycle.Event.Name.OnDestroy)
+    }
+
+    private fun List<Lifecycle.Event>.assertDoesNotContain(className: String, name: Lifecycle.Event.Name) {
+        var error: AssertionError? = null
+        try {
+            assertContains(className, name)
+            error = AssertionError("LCEs contained $name for $className")
+        } catch (_: AssertionError) {
+            // pass
+        }
+
+        error?.let { throw it }
+    }
+
+    private fun List<Lifecycle.Event>.assertContains(className: String, name: Lifecycle.Event.Name) {
+        for (lce in this) {
+            if (lce.className != className) continue
+            if (lce.name == name) return
+        }
+
+        fail("LCEs did not contain $name for $className")
     }
 }
