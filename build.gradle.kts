6f9a00645e66a2747289f0a39bc5e66e3c307b50
Matthew Nelson <developer@matthewnelson.io>
2024-12-01 15:11:08 -0500

    Prepare for next development iteration

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  **/
+import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnLockMismatchReport
 import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnPlugin
 import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnRootExtension
 
@@ -45,12 +46,18 @@ allprojects {
 
 }
 
+@Suppress("PropertyName")
+val CHECK_PUBLICATION = findProperty("CHECK_PUBLICATION") != null
+
 plugins.withType<YarnPlugin> {
     the<YarnRootExtension>().lockFileDirectory = rootDir.resolve(".kotlin-js-store")
+    if (CHECK_PUBLICATION) {
+        the<YarnRootExtension>().yarnLockMismatchReport = YarnLockMismatchReport.NONE
+    }
 }
 
 apiValidation {
-    if (findProperty("CHECK_PUBLICATION") != null) {
+    if (CHECK_PUBLICATION) {
         ignoredProjects.add("check-publication")
     } else {
         nonPublicMarkers.add("io.matthewnelson.kmp.tor.common.api.InternalKmpTorApi")
