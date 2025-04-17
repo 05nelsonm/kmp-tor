d8a66891483391223ff471e691b539712941f5b6
Matthew Nelson <developer@matthewnelson.io>
2025-04-17 08:52:19 -0400

    Add io.matthewnelson.kmp.process as required module

--- library/runtime-ctrl/src/jvmMain/java9/module-info.java
+++ library/runtime-ctrl/src/jvmMain/java9/module-info.java
@@ -1,6 +1,7 @@
 module io.matthewnelson.kmp.tor.runtime.ctrl {
     requires io.matthewnelson.immutable.collections;
     requires transitive io.matthewnelson.kmp.file;
+    requires io.matthewnelson.kmp.process;
     requires io.matthewnelson.kmp.tor.common.core;
     requires transitive io.matthewnelson.kmp.tor.runtime.core;
     requires kotlinx.coroutines.core;
