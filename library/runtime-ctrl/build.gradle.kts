fb465243884c1056fabfffa4ebc72491cd52f1ab
Matthew Nelson <developer@matthewnelson.io>
2025-05-26 09:26:00 -0400

    Update kmp-tor-resource: 408.16.1 -> 408.16.4-SNAPSHOT

--- library/runtime-ctrl/build.gradle.kts
+++ library/runtime-ctrl/build.gradle.kts
@@ -25,7 +25,8 @@ kmpConfiguration {
         js {
             sourceSetTest {
                 dependencies {
-                    implementation(npm("kmp-tor.resource-exec-tor.all", libs.versions.kmp.tor.resource.get()))
+                    // TODO: REMOVE[kmp-tor-resource] remove .0 snapshot version
+                    implementation(npm("kmp-tor.resource-exec-tor.all", libs.versions.kmp.tor.resource.get() + ".0"))
                 }
             }
         }
