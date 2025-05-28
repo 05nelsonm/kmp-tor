8552435ae0666be70b599db862c1318521e302a5
Matthew Nelson <developer@matthewnelson.io>
2025-05-28 08:20:22 -0400

    Use System.out on android native executable tests to ensure output is captured by logcat

--- test-android/src/androidInstrumentedTest/kotlin/io/matthewnelson/kmp/tor/test/android/AndroidNativeTest.kt
+++ test-android/src/androidInstrumentedTest/kotlin/io/matthewnelson/kmp/tor/test/android/AndroidNativeTest.kt
@@ -48,9 +48,14 @@ class AndroidNativeTest {
     private fun run(libName: String, timeout: Duration) {
         val process = Process.Builder(nativeLibraryDir.resolve(libName))
             .stdin(Stdio.Null)
-            .stdout(Stdio.Inherit)
-            .stderr(Stdio.Inherit)
-            .spawn { p -> p.waitFor(duration = timeout); p }
+            .spawn { p ->
+                p.stdoutFeed { line ->
+                    println(line ?: "STDOUT: END")
+                }.stderrFeed { line ->
+                    System.err.println(line ?: "STDERR: END")
+                }.waitFor(duration = timeout)
+                p
+            }
 
         println("RUN_LENGTH[${process.startTime.elapsedNow().inWholeSeconds}s]")
         assertEquals(0, process.waitFor())
