a59c869932c95b856e6634c7b2ef748de1c65ec3
Matthew Nelson <developer@matthewnelson.io>
2025-09-18 07:07:54 -0400

    WIP: Migrate UnixSocketNotSupportedMessage to jsWasmJs source set

--- library/runtime-core/src/jsWasmJsMain/kotlin/io/matthewnelson/kmp/tor/runtime/core/internal/JsWasmJsPlatform.kt
+++ library/runtime-core/src/jsWasmJsMain/kotlin/io/matthewnelson/kmp/tor/runtime/core/internal/JsWasmJsPlatform.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Matthew Nelson
+ * Copyright (c) 2025 Matthew Nelson
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,15 +13,23 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  **/
-@file:Suppress("NOTHING_TO_INLINE")
-
 package io.matthewnelson.kmp.tor.runtime.core.internal
 
-import io.matthewnelson.kmp.process.InternalProcessApi
+import io.matthewnelson.kmp.file.SysDirSep
+import io.matthewnelson.kmp.tor.common.api.InternalKmpTorApi
+import io.matthewnelson.kmp.tor.common.core.OSHost
+import io.matthewnelson.kmp.tor.common.core.OSInfo
 
-@OptIn(InternalProcessApi::class)
-internal inline fun net_Server.onError(
-    noinline callback: (err: dynamic) -> Unit,
-) {
-    on("error", callback)
+@OptIn(InternalKmpTorApi::class)
+internal actual val UnixSocketsNotSupportedMessage: String? by lazy {
+    val host = OSInfo.INSTANCE.osHost
+    if (host is OSHost.Windows) {
+        return@lazy "Tor does not support Unix Sockets on Windows"
+    }
+
+    if (SysDirSep != '/') {
+        "Unsupported OSHost[$host]"
+    } else {
+        null
+    }
 }
